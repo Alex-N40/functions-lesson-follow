5da6d3a5d66ae65c5bba52e085e0ed458b3e8282
Alex-N40 <alex_n40@proton.me>
2024-04-25 12:16:53 -0500

    First commit

---
+++ script.js
@@ -0,0 +1,18 @@
+var welcome = function (name) {
+  console.log(`Welcome ${name}. Have a great day!`);
+};
+welcome("Vera");
+welcome("Adrian");
+welcome("Aida");
+
+var addTogether = function (num1, num2) {
+  //console.log(num1 + num2);
+  return num1 + num2;
+};
+/*addTogether(5, 7);
+addTogether(13, 72);
+addTogether(36, -2.88);*/
+//alert(addTogether(36, -2.88));
+console.log(addTogether(13, 72));
+var lunchForTwo = addTogether(24.56, 18.99);
+console.log(lunchForTwo);
